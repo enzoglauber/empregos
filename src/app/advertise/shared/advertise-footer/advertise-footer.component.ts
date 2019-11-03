b7493ae6e4af94e6a2eaae737130a1db566589db
Enzo Glauber <enzojob@gmail.com>
2019-11-03 13:07:20 -0300

    Copyright

--- src/app/advertise/shared/advertise-footer/advertise-footer.component.ts
+++ src/app/advertise/shared/advertise-footer/advertise-footer.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advertise-footer.component.scss']
 })
 export class AdvertiseFooterComponent implements OnInit {
+  year: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.year = new Date().getFullYear();
   }
 
 }
