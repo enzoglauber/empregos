6e5485fca1deed9caa604e28cb201623d32269d9
Enzo Glauber <enzojob@gmail.com>
2019-11-04 23:13:46 -0300

    Form validate

--- src/app/advertise/shared/advertise-register-form/advertise-register-form.component.ts
+++ src/app/advertise/shared/advertise-register-form/advertise-register-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AdvertiseService } from '../advertise.service';
 import { Observable } from 'rxjs';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-advertise-register-form',
@@ -9,10 +10,8 @@ import { Observable } from 'rxjs';
   encapsulation: ViewEncapsulation.None
 })
 export class AdvertiseRegisterFormComponent implements OnInit {
-  advertise: any = {
-    responsibility: 0,
-    city: 0,
-  };
+  form: FormGroup;
+  isSubmitted = false;
 
   carouselOptions = {
     margin: 0,
@@ -47,6 +46,25 @@ export class AdvertiseRegisterFormComponent implements OnInit {
     this.slides = this.advertiseService.getSlides();
     this.responsibilities$ = this.advertiseService.getResponsibilities();
     this.cities$ = this.advertiseService.getCities();
+    // 
+    this.form = new FormGroup({
+      name: new FormControl(null, [Validators.required] ), 
+      email: new FormControl(null, [Validators.required] ), 
+      cellphone: new FormControl(null, [Validators.required] ), 
+      city: new FormControl(0, [Validators.required] ), 
+      responsibility: new FormControl(0, [Validators.required] ), 
+    }, { updateOn: 'blur' });
   }
 
+  save (): void {
+    this.isSubmitted = true;
+    if(this.form.invalid){
+      return;
+    }
+    console.log('Enviando dados', this.form.value)
+  }
+
+  get formControls() { 
+    return this.form.controls; 
+  }
 }
--- src/app/advertise/shared/advertise-shared.module.ts
+++ src/app/advertise/shared/advertise-shared.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { OwlModule } from 'ngx-owl-carousel';
 
 import { AdvertiseFooterComponent } from './advertise-footer/advertise-footer.component';
 import { AdvertiseHeaderComponent } from './advertise-header/advertise-header.component';
 import { AdvertiseRegisterFormComponent } from './advertise-register-form/advertise-register-form.component';
 import { AdvertiseService } from './advertise.service';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +15,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     OwlModule
   ],
   exports: [
